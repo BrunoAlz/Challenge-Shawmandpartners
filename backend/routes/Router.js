f41512903e40cddfc0cd755357518d892c42113c
Bruno Alves <bruno6821@gmail.com>
2023-05-10 20:36:31 -0300

    Implement user routes file

--- backend/routes/Router.js
+++ backend/routes/Router.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const router = express();
 
-
-router.get("/", (req, res) => {
-  res.send("API WORKING");
-});
+router.use("/api/user", require("./UserRoutes"));
 
 module.exports = router;
