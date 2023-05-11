ad6cc1fd8200c9ff8a068afe13454b328c3456da
Bruno Alves <bruno6821@gmail.com>
2023-05-11 14:58:49 -0300

    Componentizing the List Cards

---
+++ frontend/src/components/UserCard.jsx
@@ -0,0 +1,26 @@
+import Card from "react-bootstrap/Card";
+import { Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+const UserCard = ({ data }) => {
+  return (
+    <>
+      {data.map((user) => (
+        <Col key={user.id} sm={12} md={6} lg={4} xl={3}>
+          <Link to={`/user/${user.login}`} className="text-decoration-none">
+            <Card className="mt-4" style={{ width: "18rem" }}>
+              <Card.Body>
+                <Card.Title className="text-center fs-4">
+                  {user.login}
+                </Card.Title>
+                <Card.Subtitle className="mb-2">ID: {user.id}</Card.Subtitle>
+              </Card.Body>
+            </Card>
+          </Link>
+        </Col>
+      ))}
+    </>
+  );
+};
+
+export default UserCard;
--- frontend/src/pages/UsersList.jsx
+++ frontend/src/pages/UsersList.jsx
@@ -1,10 +1,11 @@
-import Card from "react-bootstrap/Card";
-import { useState, useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+
 import { getUsers } from "../slices/userSlice";
-// BOOSTRAP
-import { Row, Col } from "react-bootstrap";
-import { Link } from "react-router-dom";
+
+import { Row } from "react-bootstrap";
+
+import UserCard from "../components/UserCard";
 
 const UsersList = () => {
   const { data, error, success, loading } = useSelector((state) => state.user);
@@ -18,20 +19,7 @@ const UsersList = () => {
   return (
     <div>
       <Row>
-        {data.map((user) => (
-          <Col key={user.id} sm={12} md={6} lg={4} xl={3}>
-            <Link to={`/user/${user.login}`} className="text-decoration-none">
-              <Card className="mt-4" style={{ width: "18rem" }}>
-                <Card.Body>
-                  <Card.Title className="text-center fs-4">
-                    {user.login}
-                  </Card.Title>
-                  <Card.Subtitle className="mb-2">ID: {user.id}</Card.Subtitle>
-                </Card.Body>
-              </Card>
-            </Link>
-          </Col>
-        ))}
+        <UserCard data={data} />
       </Row>
     </div>
   );
