7d21dd36da103efe0713b3270bd859f9a6c02fb1
Bruno Alves <bruno6821@gmail.com>
2023-05-12 09:53:49 -0300

    Header link adjustment

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,4 +1,5 @@
 import { Container, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 
 const Header = () => {
@@ -6,9 +7,9 @@ const Header = () => {
     <header>
       <Navbar className="shadow" bg="light" expand="lg" collapseOnSelect>
         <Container fluid>
-            <Navbar.Brand className="fs-3 p-0">
-             GITHUB USERS API
-            </Navbar.Brand>          
+          <Navbar.Brand className="fs-3 p-0">
+            <Link className="text-decoration-none fw-bold text-dark">GITHUB USERS API</Link>
+          </Navbar.Brand>
         </Container>
       </Navbar>
     </header>
