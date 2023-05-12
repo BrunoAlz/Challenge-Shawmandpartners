a8cab3eaf0fbdfcfc47bd148fffc90a5c5b9ba73
Bruno Alves <bruno6821@gmail.com>
2023-05-12 17:21:47 -0300

    Fixing bugs before Deploy

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 // Configuration of Responses in JSON and Form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({ credentials: true, origin: "http://localhost:5173" }));
+app.use(cors({ credentials: true, origin: "*" }));
 
 // Configuring Routes
 const router = require("./routes/Router.js");
