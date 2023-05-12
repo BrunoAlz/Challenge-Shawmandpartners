f0861691c6095bd6f61a11001502135f9016b1e1
Bruno Alves <bruno6821@gmail.com>
2023-05-12 13:48:20 -0300

    Adjusting backend repos pagination

--- backend/controllers/userControler.js
+++ backend/controllers/userControler.js
@@ -1,9 +1,13 @@
 const axios = require("axios");
-const { parseNextLink, extractSinceFromLink } = require("../utils/pagination");
+const {
+  parseNextLink,
+  extractSinceFromLink,
+  getNextPageLinkForRepos,
+} = require("../utils/pagination");
 require("dotenv").config();
 const token = process.env.TOKEN;
 
-// Controller for listing users
+// list users Controler
 const getUsers = async (req, res) => {
   try {
     const { since } = req.query;
@@ -31,7 +35,7 @@ const getUsers = async (req, res) => {
   }
 };
 
-// Controller for user details
+// get user details Controller
 const getUserDetail = async (req, res) => {
   try {
     const { username } = req.params;
@@ -51,13 +55,15 @@ const getUserDetail = async (req, res) => {
   }
 };
 
-// Controller for user Repos
+// get User Repos
 const getUserRepos = async (req, res) => {
   try {
     const { username } = req.params;
+    const { page } = req.query;
+    const perPage = 15;
 
     const response = await axios.get(
-      `https://api.github.com/users/${username}/repos`,
+      `https://api.github.com/users/${username}/repos?page=${page}&per_page=${perPage}`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
@@ -65,7 +71,13 @@ const getUserRepos = async (req, res) => {
       }
     );
 
-    res.json(response.data);
+    const linkHeader = response.headers.link;
+    const nextPageLink = getNextPageLinkForRepos(linkHeader);
+
+    res.json({
+      repos: response.data,
+      nextPage: nextPageLink,
+    });
   } catch (error) {
     res.status(404).json({ error: "Error user does not exist" });
   }
--- backend/utils/pagination.js
+++ backend/utils/pagination.js
@@ -5,8 +5,6 @@ const parseNextLink = (linkHeader) => {
 
   const links = linkHeader.split(", ");
   const nextPageLink = links.find((link) => link.includes('rel="next"'));
-  const prevPageLink = links.find((link) => link.includes('rel="prev"'));
-  console.log(links);
   if (!nextPageLink) {
     return null;
   }
@@ -25,10 +23,40 @@ const extractSinceFromLink = (link) => {
   }
 
   const params = new URLSearchParams(new URL(link).search);
-  return params.get("since");
+
+  if (params.has("since")) {
+    return params.get("since");
+  } else if (params.has("page")) {
+    const page = params.get("page");
+    const perPage = params.get("per_page");
+    const since = (parseInt(page) - 1) * parseInt(perPage);
+    return since.toString();
+  }
+
+  return null;
+};
+
+const getNextPageLinkForRepos = (linkHeader) => {
+  if (!linkHeader) {
+    return null;
+  }
+
+  const links = linkHeader.split(", ");
+  const nextPageLink = links.find((link) => link.includes('rel="next"'));
+  if (!nextPageLink) {
+    return null;
+  }
+
+  const match = nextPageLink.match(/<([^>]+)>/);
+  if (!match) {
+    return null;
+  }
+
+  return match[1];
 };
 
 module.exports = {
   parseNextLink,
   extractSinceFromLink,
+  getNextPageLinkForRepos,
 };
